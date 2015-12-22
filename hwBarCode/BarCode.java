d6b3b9772cd043272b23d8aa5fcb65021c012721
Karol-Regula <kregula@stuy.edu>
2015-12-22 12:21:15 -0500

    checkSum Works

--- hwBarCode/BarCode.java
+++ hwBarCode/BarCode.java
@@ -1,38 +1,57 @@
 public class BarCode implements Comparable{
-    // instance variables
     private String _zip;
     private int _checkDigit;
+    private final static String[] reference = new String[10];
     
     // constructors
     //precondtion: zip.length() = 5 and zip contains only digits.
     //postcondition: throws a runtime exception zip is not the correct length
     //               or zip contains a non digit
     //               _zip and _checkDigit are initialized.
-    public BarCode(String zip) {}
+    public BarCode(String zip){
+	_zip = zip;
+	_checkDigit = checkSum();
+    }
     
     // postcondition: Creates a copy of a bar code.
-    public BarCode(BarCode x){}
+    public BarCode(BarCode x){
+	
+    }
     
-    
-    //post: computes and returns the check sum for _zip
-    private int checkSum(){}
+    private int checkSum(){//checksum works
+	int a = Integer.parseInt(_zip.substring(0,1));
+	int b = Integer.parseInt(_zip.substring(1,2));
+	int c = Integer.parseInt(_zip.substring(2,3));
+	int d = Integer.parseInt(_zip.substring(3,4));
+	int e = Integer.parseInt(_zip.substring(4,5));
+	return (a + b + c + d + e)%10;
+    }
     
     //postcondition: format zip + check digit + barcode 
     //ex. "084518  |||:::|::|::|::|:|:|::::|||::|:|"      
-    public String toString(){}
+    public String toString(){
+	return "";
+    }
     
     
-    public boolean equals(Object other){}
+    public boolean equals(Object other){
+	return true;
+    }
     // postcondition: false if the object is not a BarCode, 
     // false if it is a non-matching barcode
     // true when they match.
     
     
-    public int compareTo(Comparable other){}
+    public int compareTo(Object other){
+	return 0;
+    }
     // postcondition: compares the zip + checkdigit 
     
     public static void main(String[]args){
-	
+	BarCode b1 = new BarCode("12345");
+	BarCode b2 = new BarCode("75420");
+	System.out.println(b1.checkSum());
+	System.out.println(b2.checkSum());
     }
     
 }
